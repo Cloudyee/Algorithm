66f221287b434a1c358e5c819a99781e7ee41d9d
gyongyeon <126961013+Cloudyee@users.noreply.github.com>
2023-12-26 23:53:02 +0900

    [Gold IV] Title: 연속 합, Time: 220 ms, Memory: 17536 KB -BaekjoonHub

--- 백준/Gold/2737. 연속 합/연속 합.java
+++ 백준/Gold/2737. 연속 합/연속 합.java
@@ -3,26 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int testCases = sc.nextInt();
 
-        int T = sc.nextInt(); // 테스트 케이스의 개수
-        while (T-- > 0) {
-            double n = sc.nextDouble(); // 입력받은 수
+        while (testCases-- > 0) {
+            long target = sc.nextLong();
+            int count = 0;
 
-            // k의 최댓값을 계산
-            double lim = (-1 + Math.sqrt(8 * n + 1)) / 2;
-
-            int ans = 0; // 결과 초기화
-            for (double k = 2; k <= lim; k++) {
-                // 연속된 자연수의 합을 계산
-                double a = -(k / 2) + 0.5 + (n / k);
-                
-                // a가 정수인지 확인하여 결과 증가
-                if (a == (int) a) {
-                    ans++;
+            for (long k = 2; k * (k + 1) / 2 <= target; k++) {
+                if ((target - k * (k + 1) / 2) % k == 0) {
+                    count++;
                 }
             }
 
-            System.out.println(ans); // 결과 출력
+            System.out.println(count);
         }
     }
 }
