e600589cc756909be0f25acbd9c7838036449514
Cloudyee <126961013+Cloudyee@users.noreply.github.com>
2024-05-09 23:45:28 +0900

    [Bronze II] Title: 피보나치 수, Time: 204 ms, Memory: 17684 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.java
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -6,21 +6,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        D = new int[n+1];
-        for(int i = 0 ; i<=n ; i++){
+        D = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
             D[i] = -1;
         }
-        D[0]=0;
+        D[0] = 0;
         D[1] = 1;
-        fibo(n);
+        for(int i = 2; i<=n ; i++){
+            D[i] = D[i-1]+D[i-2];
+        }
         System.out.println(D[n]);
-    }
-
-    static int fibo(int n){
-        //기존에 계산한 적이 있는 부분의 문제는 재계산 하지않고 리턴
-        if(D[n] != -1)
-            return D[n];
-        return D[n] = fibo(n-2)+fibo(n-1);
-        //메모이제이션 : 구한값을 바로 리턴하지 않고 DP 테이블에 저장한 후 리턴하도록 로직을 구현
     }
 }
