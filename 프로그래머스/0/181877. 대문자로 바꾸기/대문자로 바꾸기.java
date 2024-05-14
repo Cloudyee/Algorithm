763903db069f6e3cc048d66d4d8022ac141f02f9
Cloudyee <126961013+Cloudyee@users.noreply.github.com>
2024-05-14 23:58:09 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 0.03 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.java
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -1,13 +1,6 @@
 class Solution {
     public String solution(String myString) {
-        String answer = "";
-        for(int i = 0 ; i<myString.length() ; i++){
-            if(Character.isUpperCase(myString.charAt(i))){
-                answer+=myString.charAt(i);
-            }else{
-                answer+= Character.toUpperCase(myString.charAt(i));
-            }
-        }
+        String answer = myString.toUpperCase();
         return answer;
     }
 }
