569f984e44156ea0df5409ca331fc5f3a9912b87
Cloudyee <126961013+Cloudyee@users.noreply.github.com>
2024-05-14 23:47:15 +0900

    [level 0] Title: 5명씩, Time: 0.02 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String[] solution(String[] names) {
+        String[] answer = new String[(names.length+4)/5];
+        int cnt=0;
+        for(int i = 0 ; i< names.length; i+=5){
+            answer[cnt]=names[i];
+            cnt++;
+            if (i+5>names.length)
+                break;
+        }
+        return answer;
+    }
+}
