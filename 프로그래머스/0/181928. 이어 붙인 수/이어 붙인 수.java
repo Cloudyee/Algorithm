655c5d0459d36712737d5b9a29892bdb69a42d57
Cloudyee <126961013+Cloudyee@users.noreply.github.com>
2024-05-17 22:49:29 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.04 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -1,17 +1,17 @@
 class Solution {
     public int solution(int[] num_list) {
         int answer = 0;
-        StringBuilder add = new StringBuilder();
+        StringBuilder even = new StringBuilder();
         StringBuilder odd = new StringBuilder();
 
         for (int i = 0; i < num_list.length; i++) {
             if (num_list[i] % 2 == 0)
-                add.append(num_list[i]);
+                even.append(num_list[i]);
             else
                 odd.append(num_list[i]);
         }
         
-        answer=Integer.parseInt(add.toString())+ Integer.parseInt(odd.toString());
+        answer=Integer.parseInt(even.toString())+ Integer.parseInt(odd.toString());
         return answer;
     }
 }
