9282cb84380bd2cbe78c33a346eb03765595c23f
Cloudyee <126961013+Cloudyee@users.noreply.github.com>
2024-05-20 00:18:01 +0900

    [level 0] Title: 배열 만들기 2, Time: 0.14 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int l, int r) {
+        List<Integer> results = new ArrayList<>();
+        
+        //BFS를 사용하여 0과 5로만 이루어진 숫자를 생성한다.
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(0);
+        queue.add(5);
+        while(!queue.isEmpty()){
+            //queue의 맨 앞의 값 반환 후 삭제
+            int num = queue.poll();
+            
+            //num이 0인 경우는 넘기고, 범위 내에 있는 숫자만 리스트에 추가한다.
+            if(num!=0 && num>=l && num<=r){
+                results.add(num);
+            }
+            
+            //num*10+0과 num*10+5를 큐에 추가
+            if(num!=0){
+                int nextNum1 = num*10;
+                int nextNum2 = nextNum1+5;
+                
+                if(nextNum1<=r) queue.add(nextNum1);
+                if(nextNum2<=r) queue.add(nextNum2);
+            }
+        }
+        
+        if(results.isEmpty())
+            return new int[]{-1};
+        
+        //결과를 오름차순으로 정렬한 후 배열로 변환
+        Collections.sort(results);
+        return results.stream().mapToInt(i->i).toArray();
+    }
+}
