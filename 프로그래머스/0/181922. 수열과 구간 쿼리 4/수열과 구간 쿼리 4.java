2b01a47fd89b861bd735078d84b2641be3742d9b
Cloudyee <126961013+Cloudyee@users.noreply.github.com>
2024-05-19 23:23:53 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 1.46 ms, Memory: 82.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        for(int[] querie : queries){
+            int s = querie[0];
+            int e = querie[1];
+            int k = querie[2];
+            
+            for(int i = s ;  i<=e ; i++){
+                if(i%k==0){
+                    arr[i]+=1;
+                }
+            }
+        }
+        return arr;
+    }
+}
