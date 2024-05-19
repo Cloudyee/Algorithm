f7a57dd837781384c884963cc049cf4d28265c1a
Cloudyee <126961013+Cloudyee@users.noreply.github.com>
2024-05-20 00:20:46 +0900

    [level 0] Title: 배열 만들기 2, Time: 0.16 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -35,3 +35,21 @@ class Solution {
         return results.stream().mapToInt(i->i).toArray();
     }
 }
+
+
+//-----------------------------------
+//다른분의 정말 멋진 풀이
+// class Solution {
+//     public int[] solution(int l, int r) {
+
+//         ArrayList<Integer> list = new ArrayList<>();
+
+//         for (int i = 1; i < 64; i++) {
+//             int num = Integer.parseInt(Integer.toBinaryString(i)) * 5;
+//             if (l <= num && num <= r)
+//                 list.add(num);
+//         }
+
+//         return list.isEmpty() ? new int[] { -1 } : list.stream().mapToInt(i -> i).toArray();
+//     }
+// }
