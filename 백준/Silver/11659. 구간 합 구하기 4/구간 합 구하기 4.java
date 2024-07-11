34e12033e8a62a034a81d75580b3e218e136d02b
Cloudyee <126961013+Cloudyee@users.noreply.github.com>
2024-07-11 23:10:43 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1164 ms, Memory: 63472 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -7,24 +7,23 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        //데이터의 갯수 입력
-        int suNo = Integer.parseInt(st.nextToken());
-        //질의 갯수 입력
-        int quizNo = Integer.parseInt(st.nextToken());
-        //구간합을 구할 대상 배열 저장용
-        long[] S = new long[suNo+1];
+
+        int N = Integer.parseInt(st.nextToken()); //수의 갯수
+        int M = Integer.parseInt(st.nextToken()); //합을 구해야하는 횟수
+
+        long [] sum = new long [N+1];
         st = new StringTokenizer(br.readLine());
-        for(int i = 1 ; i<=suNo;i++){
-            //입력 받은 값들의 구간합들을 저장
-            S[i] = S[i-1] + Integer.parseInt(st.nextToken());
+        for(int i = 1; i<=N ; i++){
+            sum[i] = sum[i-1] + Long.parseLong(st.nextToken());
         }
-        for(int q = 0; q<quizNo;q++){
-            //구간을 입력받기 위한 토크나이저
+
+        int start = 0;
+        int end = 0;
+        for(int i = 0 ; i<M ; i++){
             st = new StringTokenizer(br.readLine());
-            int i = Integer.parseInt(st.nextToken());
-            int j = Integer.parseInt(st.nextToken());
-            //구간합 출력
-            System.out.println(S[j]-S[i-1]);
+            start = Integer.parseInt(st.nextToken());
+            end = Integer.parseInt(st.nextToken());
+            System.out.println(sum[end] - sum[start - 1]);
         }
     }
 }
