fd4dd306d3f5423291a0ed762fdd2c75f9044f9d
Cloudyee <126961013+Cloudyee@users.noreply.github.com>
2024-07-19 00:33:54 +0900

    [Gold III] Title: 나머지 합, Time: 540 ms, Memory: 120552 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -1,43 +1,49 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        
-        // 수열의 개수
-        int N = sc.nextInt();
-        // 나누어 떨어지는 수
-        int M = sc.nextInt();
-        
-        long[] S = new long[N]; // 수정: 배열 크기를 N으로 변경
-        long[] C = new long[M];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        //입력받을 수의 개수
+        int N = Integer.parseInt(st.nextToken());
+        //나눌 값
+        int M = Integer.parseInt(st.nextToken());
+        //구간합 배열
+        long[] sum = new long[N];
+        //구간합 % M 배열 : 인덱스 = 나머지 값의 갯수
+        long[] remain = new long[M];
+
         long answer = 0;
-        S[0] = sc.nextInt();
         
-        // 수열 합 배열 만들기
-        for (int i = 1; i < N; i++) {
-            S[i] = S[i - 1] + sc.nextInt();
+        st = new StringTokenizer(br.readLine());
+        sum[0] = Long.parseLong(st.nextToken());
+        //구간합 저장
+        for(int i = 1 ; i<N ; i++){
+            sum[i] = sum[i-1] + Integer.parseInt(st.nextToken());
         }
-        
-        // 합 배열의 모든 값에 % 연산 수행
-        for (int i = 0; i < N; i++) {
-            int remainder = (int) (S[i] % M);
-            // 구간합이 0일 때 정답에 더해주기
-            if (remainder == 0) answer++;
-            // 나머지가 같은 인덱스 수 카운팅
-            C[remainder]++;
+
+        for(int i = 0; i<N ; i++){
+            int remainder = (int) (sum[i] % M);
+            //0~i까지 구간합 자체가 0일때 정답에 더하기
+            if(remainder == 0)
+                answer++;
+            remain[remainder]++;
         }
-        
-        for (int i = 0; i < M; i++) {
-            // 나머지가 같은 인덱스 중 2개를 뽑는 경우의 수 더하기
-            if (C[i] > 1) {
-                answer = answer + (C[i] * (C[i] - 1) / 2);
+
+        for(int i = 0 ; i<M ; i++){
+            if(remain[i] > 1){
+                //나머지가 같은 인덱스 중 2개를 뽑는 경우의 수를 더하기
+                // -> 위에서 0을 더해주는것이 중복이 되지 않는가?
+                // => 그렇지 않다. 위의 수를 더해준 것은 0인덱스부터 자신의 인덱스까지의 합 % M이 나누어떨어지는 경우를 더한것.
+                // 지금의 경우는 나머지가 같은 인덱스 중 2가지를 뽑는 경우의 수를 더한 것.
+
+                //아래 식은 조합의 식 n*(n-1)/2
+                answer = answer+ (remain[i] * (remain[i]-1))/2;
             }
         }
         System.out.println(answer);
-        
-        // 수정: Scanner 닫기
-        sc.close();
     }
 }
