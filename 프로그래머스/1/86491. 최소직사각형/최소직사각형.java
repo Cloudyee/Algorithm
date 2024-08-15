21da91a651456f281c3067eda1e3d1a013866438
Cloudyee <126961013+Cloudyee@users.noreply.github.com>
2024-08-15 22:23:49 +0900

    [level 1] Title: 최소직사각형, Time: 1.93 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -2,17 +2,20 @@ class Solution {
     public int solution(int[][] sizes) {
         //x는 두 값 중 더 큰 값을 갖는다.
         int x = Integer.MIN_VALUE;
-        //y눈 듀 겂 즁 도 작은 값을 갖는다.
+        //y는 두 값 중 더 작은 값을 갖는다.
         int y = Integer.MIN_VALUE;
         for(int[] wallet :  sizes){
-            if(wallet[0] > wallet[1]){
-                x = Math.max(wallet[0] , x);
-                y = Math.max(wallet[1] , y); 
-            }else{
-                x = Math.max(wallet[1] , x);
-                y = Math.max(wallet[0] , y);
-            }
+            // if(wallet[0] > wallet[1]){
+            //     x = Math.max(wallet[0] , x);
+            //     y = Math.max(wallet[1] , y); 
+            // }else{
+            //     x = Math.max(wallet[1] , x);
+            //     y = Math.max(wallet[0] , y);
+            // }
             
+            //아래는 다른 사람들 것을 참고하여 더 최적화한 것
+            x = Math.max(Math.max(wallet[0], wallet[1]), x);
+            y = Math.max(Math.min(wallet[0], wallet[1]), y);
         }
         int answer = x*y;
         return answer;
